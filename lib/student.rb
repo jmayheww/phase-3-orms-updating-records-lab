bded32a7b9e3af54f69ca09ce066c7f0adfa880a
Joshua <joshsmayhew@gmail.com>
2022-12-11 13:27:34 +0700

    commit tree c4a1f8fd6ca395818c168b5fcdc59d84d4fcad5b

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,95 @@
-require_relative "../config/environment.rb"
+require_relative '../config/environment'
 
+require 'pry'
 class Student
+  attr_accessor :name, :grade, :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
 
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+        DROP TABLE IF EXISTS students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if id
+      update
+
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+
+      # insert the song
+      DB[:conn].execute(sql, name, grade)
+
+      # get the song ID from the database and save it to the Ruby instance
+      self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM students')[0][0]
+
+      # return the Ruby instance
+      self
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+
+    new(name, grade, id)
+  end
+
+  def self.all
+    sql = <<-SQL
+        SELECT *
+        FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = 'UPDATE students SET name = ?, grade = ? WHERE id = ?'
+    DB[:conn].execute(sql, name, grade, id)
+  end
 end
